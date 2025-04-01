1ff3b8bbfe78ce491d5b7abeb4709a40e0c5c41c
Chalmers Ong <142716318+chalmetrs@users.noreply.github.com>
2025-04-01 11:11:38 +0200

    Update t7_functions.py

--- t7_functions.py
+++ t7_functions.py
@@ -155,8 +155,7 @@ def get_embeddings(graphs, model, device='cpu'):
 
         # Copy graph and get ID
         G = copy.deepcopy(graph)
-        # id = G.graph["name"]
-        id = G.graph.get("name", "graph")
+        id = G.graph["ID"]
 
         # Convert to PyG graph
         G = remove_attributes_from_graph(G, ["polygon"])
